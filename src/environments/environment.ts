a9404066599f04cf990093ff9a3598a4a3777d39
shrikantk3 <shrikant.kumar0402@gmail.com>
2020-05-13 16:42:46 +0530

    Add mytutor file

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDu-wMuTs6FJoAWby3UHv5LP2vmTkdXHAo",
+  authDomain: "mytutor-e7ae0.firebaseapp.com",
+  databaseURL: "https://mytutor-e7ae0.firebaseio.com",
+  projectId: "mytutor-e7ae0",
+  storageBucket: "mytutor-e7ae0.appspot.com",
+  messagingSenderId: "81756765532",
+  appId: "1:81756765532:web:61838faf24ed1086f7d599",
+  measurementId: "G-GMEESMVLNG"
+};
